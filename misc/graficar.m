675a5b4e76add2e787543ed701481ceadb9593aa
javierbrk <javierbrk@abfeffc9-348a-3d18-b339-35a71ad973c7>
2011-08-17 01:13:31 +0000

    graficas de matlab

---
+++ misc/graficar.m
@@ -0,0 +1,11 @@
+tx = ty = linspace (1, 100, 41)';
+[xx, yy] = meshgrid (tx, ty);
+tz = (-0.01 .*xx + 0.48) .* yy - 0.03;
+mesh (tx, ty, tz);
+xlabel("k");
+ylabel("tiempo por tarea en horas");
+zlabel ("paga maxima por tarea");
+view(9,35);
+print('-dsvg','figure.svg');
+print('-dpsc','figure.ps');
+print('-djpg','figure.jpg');
